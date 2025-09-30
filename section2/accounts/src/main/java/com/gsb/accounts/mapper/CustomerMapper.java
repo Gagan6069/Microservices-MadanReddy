99be65d3e41652e0d7cbae137834f027d614b845
Gagan <bhatia.gagandeepsingh61@gmail.com>
2025-09-30 22:42:36 +0530

    section 2.22: DELETE API.

---
+++ section2/accounts/src/main/java/com/gsb/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.gsb.accounts.mapper;
+
+import com.gsb.accounts.dto.CustomerDto;
+import com.gsb.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
