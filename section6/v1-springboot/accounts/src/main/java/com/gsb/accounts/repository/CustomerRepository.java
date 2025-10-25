97becd75c1aeda674868bbbf37a96e0a1873302b
gsb <bhatia.gagandeepsingh61@gmail.com>
2025-10-26 01:32:32 +0530

    Section 6: Configuration Management in microservices
    6.69: Reading configurations using @Value annotation

---
+++ section6/v1-springboot/accounts/src/main/java/com/gsb/accounts/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.gsb.accounts.repository;
+
+import com.gsb.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+}
