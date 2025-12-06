40c5fef42d53dedebd0448cd380c80f9548c2981
gsb <bhatia.gagandeepsingh61@gmail.com>
2025-12-06 22:10:13 +0530

    Section 8.108: Setup service discovery agent using Eureka

---
+++ section8/accounts/src/main/java/com/gsb/accounts/entity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.gsb.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter @ToString
+public class BaseEntity {
+
+    @CreatedDate //Section 2.25
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy //Section 2.25
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate //Section 2.25
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy //Section 2.25
+    @Column(insertable = false)
+    private String updatedBy;
+}
