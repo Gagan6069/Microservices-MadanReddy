e551a78ae5821a373612afdd03e748b909a4d9f9
Gagan <bhatia.gagandeepsingh61@gmail.com>
2026-03-29 19:35:15 +0530

    Section 12 194. Implement Authorization inside Gateway server using Roles

---
+++ section_12/configserver/src/test/java/com/gsb/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsb.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
