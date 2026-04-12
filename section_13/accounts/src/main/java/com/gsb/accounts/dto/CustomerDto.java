1d2ac76811942f74d884c3078b25693dc13d8c63
Gagan6069 <bhatia.gagandeepsingh61@gmail.com>
2026-04-12 17:37:13 +0530

    Section 13.209 Develop message microservice using Spring Cloud Functions

---
+++ section_13/accounts/src/main/java/com/gsb/accounts/dto/CustomerDto.java
@@ -0,0 +1,43 @@
+package com.gsb.accounts.dto;
+
+//import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+// Section 2.29 @Schema
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
+public class CustomerDto {
+
+    @Schema(
+            description = "Name of the customer", example = "Gagan B."
+    )
+    @NotEmpty(message = "Name can not be a null or empty") // Section 2.24
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30") // Section 2.24
+    private String name;
+
+    @Schema(
+            description = "Email address of the customer", example = "gsb@eazybytes.com"
+    )
+    @NotEmpty(message = "Email address can not be a null or empty") // Section 2.24
+    @Email(message = "Email address should be a valid value") // Section 2.24
+    private String email;
+
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") // Section 2.24
+    private String mobileNumber;
+
+    @Schema(
+            description = "Account details of the Customer"
+    )
+    private AccountsDto accountsDto;
+}
---
+++ section_13/accounts/src/main/java/com/gsb/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package com.gsb.accounts.mapper;
+
+import com.gsb.accounts.dto.AccountsDto;
+import com.gsb.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
